d8eff9bf2c923dbadc8969fa7b470228fe41dfc2
Isaak Dyck <isaakddyck@hotmail.com>
2023-03-06 21:43:06 -0600

    Added functions for both adding and subtracting with error handling Moved the errorMessages let out of the function so it can accessed by all functions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
+let errorMessage = "";
+let errorMessage2 = "";
+
 const letExample = () => {
   const letOutput = document.getElementById("letOutput");
-  let errorMessage = "";
+
   let x = 7;
   let y = 6;
   x = prompt("Enter in a number value to multiply by 6. What happens if you don't enter a number?");
@@ -20,8 +23,38 @@ const constExample = () => {
   const x2 = 7;
   const y2 = 6;
   x2 = prompt("Enter in a number value");
+}
 
+const addExample = () => {
+  xAdd = prompt("Enter in a number for x please.");
+  if (isNaN(xAdd) || xAdd == "")
+  {
+    xAdd = 4;
+    errorMessage = (`<p class="returnStyle">Number not entered for x, defaulting to 4.</p>`);
+  }
+  yAdd = prompt("Enter in a number for y please.");
+  if (isNaN(yAdd) || yAdd == "")
+  {
+    yAdd = 4;
+    errorMessage2 = (`<p class="returnStyle">Number not entered for y, defaulting to 4.</p>`);
+  }
+    addOutput.innerHTML = (errorMessage + errorMessage2 +`<p>x = ${xAdd}, y = ${yAdd}</p> <p>${xAdd} + ${yAdd} = ${xAdd + yAdd}</p>`);
+}
 
+const subExample = () => {
+  xSub = prompt("Enter in a number for x please.");
+  if (isNaN(xSub) || xSub == "")
+  {
+    xSub = 6;
+    errorMessage = (`<p class="returnStyle">Number not entered for x, defaulting to 6.</p>`);
+  }
+  ySub = prompt("Enter in a number for y please.");
+  if (isNaN(ySub) || ySub == "")
+  {
+    ySub = 5;
+    errorMessage2 = (`<p class="returnStyle">Number not entered for y, defaulting to 5.</p>`);
+  }
+    subOutput.innerHTML = (errorMessage + errorMessage2 +`<p>x = ${xSub}, y = ${ySub}</p> <p>${xSub} + ${ySub} = ${xSub + ySub}</p>`);
 }
 
 console.log("If you can see this, than all of your code ran properly!");
